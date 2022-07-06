865b195699916829d0edaa89823a6620e94ac737
seagull30 <65073631+atome9797@users.noreply.github.com>
2022-07-06 19:25:07 +0900

    1

--- DataStructure/DataStructure/Set.cpp
+++ DataStructure/DataStructure/Set.cpp
@@ -1,202 +1,169 @@
 #include "Set.h"
 #include <iostream>
-using namespace std;
-Set::Node::Node(int data, Node*parent, Node* left, Node* right)
-	:Data(data), Parent(parent), Left(left), Right(right)
+#include <queue>
+Set::Node::Node(int data, Node* parent, Node* left, Node* right)
+    : Data(data), Parent(parent), Left(left), Right(right)
 {
-	
 }
-
-Set::Node::~Node()
-{
-	Parent = nullptr;
-	Left = nullptr;
-	Right = nullptr;
-}
-
 bool Set::Node::IsLeaf() const
 {
-	if (Left == nullptr && Right == nullptr)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    if (Left == nullptr && Right == nullptr)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
-
-
-int Set::height() const
-{
-
-	return 0;
-}
-
-int Set::height2() const
-{
-	return 0;
-}
-
 bool Set::empty() const
 {
-	if (_size == 0)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
+    if (_size == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
-
 size_t Set::size() const
 {
-	return _size;
+    return _size;
 }
-
-void Set::clear()
+bool Set::insert(int value)
 {
-	_size = 0;
-	_root = nullptr;
-}
+    // 1. 새 노드 생성
+    Node* newNode = new Node(value);
+    // 2. 루트 노드가 없을 경우 루트 노드 설정
+    if (_head->Parent == nullptr)
+    {
+        _head->Parent = newNode;
+        newNode->Parent = _head;
+        ++_size;
+        return true;
+    }
+    // 3. 삽입될 위치의 부모 노드 찾음.
+    Node* parent = nullptr;
+    Node* p = _head->Parent;
 
-pair<Set::Node*, bool> Set::insert(int value)
+    while (p)
+    {
+        parent = p;
+        if (p->Data == value)
+        {
+            return false;
+        }
+        else if (value < p->Data)
+        {
+            p = p->Left;
+        }
+        else
+        {
+            p = p->Right;
+        }
+    }
+    // 4. 새로운 노드 삽입
+    if (value < parent->Data)
+    {
+        parent->Left = newNode;
+    }
+    else
+    {
+        parent->Right = newNode;
+    }
+    newNode->Parent = parent;
+    ++_size;
+    return true;
+}
+void Set::erase(int value)
 {
-	if (empty())
-	{
-		_root = new Node(value);
-		++_size;
-
-		return make_pair(_root, true);
-	}
-
-	//이진 검색 트리 : 왼쪽 서브 트리는 자기 보다 작은 값, 오른쪽 서브 트리는 자기보다 큰값.
-	Node* currentNode = _root; //뿌리 노드 넣음
-	Node* prevNode = nullptr;
-
-	//트리의 자식끝까지 감
-	while (currentNode)
-	{
-		prevNode = currentNode;
-
-		//이진 검색 수행.
-		//같은 값이라면 해당값을 가리키는 노드와 false를 반환하면됨
-		if (currentNode->Data == value)
-		{
-			return make_pair(currentNode, false);
-		}
-		else if (currentNode->Data > value)
-		{
-			currentNode = currentNode->Left; //자식 노드로 갱신해줌
-		}
-		else
-		{
-			currentNode = currentNode->Right; //자식 노드로 갱신해줌
-		}
-	}
-
-	//currentNode에 새로운 자식 노드를 생성함 => prevNode는 부모 노드가 됨
-	currentNode = new Node(value, prevNode);
-
-	//currentNode를 prevNode에 왼쪽 자식으로 삽입할 건지 오른쪽 자식으로 삽입할건지
-	if (prevNode->Data < value)
-	{
-		prevNode->Right = currentNode; //새로 생성한 자식노드를 부모 노드가 된prevNode의 자식 노드에 할당해줌
-	}
-	else
-	{
-		prevNode->Left = currentNode; //새로 생성한 자식노드를 부모 노드가 된prevNode의 자식 노드에 할당해줌
-	}
-
-	++_size;
-	//자식노드를 새로 만들어 주는게 성공했으니까 true로 반환해줌
-	return make_pair(currentNode, true);
 }
-
 void Set::erase(Node* pos)
 {
-	//1. pos가 _head라면?
-	if (pos->Parent == nullptr)
-	{
-		//1. 자식이 없다면?
-		if (pos->IsLeaf())
-		{
-			_root = nullptr;
-			
-			delete pos;
-			pos = nullptr;
-			
-			--_size;
-			return;
-		}
-		//2. 왼쪽 자식만 있다면?
-		if (pos->Right == nullptr)
-		{
-			_root = pos->Left;
-			_root->Parent = nullptr;
-
-			delete pos;
-			pos = nullptr;
-
-			--_size;
-			return;
-		}
-		//3. 오른쪽 자식만 있다면?
-		if (pos->Left == nullptr)
-		{
-			_root = pos->Right;
-			_root->Parent = nullptr;
-
-			delete pos;
-			pos = nullptr;
-
-			--_size;
-			return;
-		}
-
-		//4. 둘다 있다면?
-		if (pos->Left != nullptr && pos->Right != nullptr)
-		{
-
-			
-		}
-	}
-	//2. pos가 리프 노드라면?
-	//3. pos가 왼쪽 자식만 있다면?
-	//4. pos가 오른족 자식만 있다면?
-	//5. pos가 자식을 둘다 가지고 있다면?
-
-
 }
-
-size_t Set::erase(int value)
+Set::Node* Set::find(int value) const
 {
-	return size_t();
+    Node* result = _head->Parent;
+    while (result)
+    {
+        if (result->Data == value)
+        {
+            break;
+        }
+        else if (result->Data < value)
+        {
+            result = result->Right;
+        }
+        else
+        {
+            result = result->Left;
+        }
+    }
+    return result;
 }
-
-Set::Node* Set::find(int value)
-{
-	return nullptr;
-}
-
-const Set::Node* Set::find(int value) const
-{
-	return nullptr;
-}
-
 void Set::traverseByPreorder() const
 {
+    traverseByPreorderHelper(_head->Parent);
 }
-
 void Set::traverseByInorder() const
 {
+    traverseByInorderHelper(_head->Parent);
 }
-
 void Set::traverseByPostorder() const
 {
+    traverseByPostorderHelper(_head->Parent);
 }
-
 void Set::traverseByLevelorder() const
 {
+    std::queue<Node*> q;
+    if (_head->Parent)
+    {
+        q.push(_head->Parent);
+    }
+    while (false == q.empty())
+    {
+        Node* node = q.front();
+        q.pop();
+        std::cout << node->Data << "->";
+        if (node->Left)
+        {
+            q.push(node->Left);
+        }
+
+        if (node->Right)
+        {
+            q.push(node->Right);
+        }
+    }
+}
+void Set::traverseByPreorderHelper(Node* node) const
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    std::cout << node->Data << "->";
+    traverseByPreorderHelper(node->Left);
+    traverseByPreorderHelper(node->Right);
+}
+void Set::traverseByInorderHelper(Node* node) const
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    traverseByInorderHelper(node->Left);
+    std::cout << node->Data << "->";
+    traverseByInorderHelper(node->Right);
+}
+void Set::traverseByPostorderHelper(Node* node) const
+{
+    if (node == nullptr)
+    {
+        return;
+    }
+    traverseByPostorderHelper(node->Left);
+    traverseByPostorderHelper(node->Right);
+    std::cout << node->Data << "->";
 }
